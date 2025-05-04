f92ff9a9bd2fc004b3783e1602cb301f33284887
youssef sameh <yousef17453@gmail.com>
2025-05-05 00:01:27 +0300

    added CORS

--- Ecom.API/Program.cs
+++ Ecom.API/Program.cs
@@ -9,6 +9,14 @@ namespace Ecom.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddCors(op =>
+                {
+                    op.AddPolicy("CORSPolicy", builder =>
+                    {
+                        builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:4200");
+                    });
+                });
             builder.Services.AddMemoryCache();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,6 +33,7 @@ namespace Ecom.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseCors("CORSPolicy");
             app.UseMiddleware<ExceptionsMiddleware>();
             // if page not found => will be redirct to error page
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
